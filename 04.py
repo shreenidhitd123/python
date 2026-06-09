a1db1afb7a222a66d70251af2f35da7cd66e6075
shreenidhitd123 <shreenidhitd123@gmail.com>
2026-06-09 23:05:09 +0530

    Create 04.py

---
+++ 04.py
@@ -0,0 +1,8 @@
+# Check if a number is even or odd using python programming language 
+
+num = int(input("Enter a number: "))
+
+if num % 2 == 0:
+    print("Even Number")
+else:
+    print("Odd Number")
