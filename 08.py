dad731cda1278929ff60b82f3372b78d95ab9e32
shreenidhitd123 <shreenidhitd123@gmail.com>
2026-06-13 00:05:24 +0530

    Create 08.py

---
+++ 08.py
@@ -0,0 +1,9 @@
+# Program to find the largest of three numbers using python programming language 
+
+a = float(input("Enter first number: "))
+b = float(input("Enter second number: "))
+c = float(input("Enter third number: "))
+
+largest = max(a, b, c)
+
+print("The largest number is:", largest)
