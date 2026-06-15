e32b60cfd10599d9300852ab9d4ce2586f88843f
shreenidhitd123 <shreenidhitd123@gmail.com>
2026-06-15 23:46:31 +0530

    Create 10.py

---
+++ 10.py
@@ -0,0 +1,16 @@
+# Armstrong Number Check using python program 
+
+num = int(input("Enter a number: "))
+temp = num
+digits = len(str(num))
+sum_of_powers = 0
+
+while temp > 0:
+    digit = temp % 10
+    sum_of_powers += digit ** digits
+    temp //= 10
+
+if num == sum_of_powers:
+    print("Armstrong Number")
+else:
+    print("Not an Armstrong Number")
