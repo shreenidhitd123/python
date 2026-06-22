af823ab7039d2d57e4a0b961a50b723d7ebbc1d2
shreenidhitd123 <shreenidhitd123@gmail.com>
2026-06-22 23:51:43 +0530

    Create 15.py

---
+++ 15.py
@@ -0,0 +1,11 @@
+# Fibonacci Series using python programming language 
+
+n = int(input("Enter the number of terms: "))
+
+a, b = 0, 1
+
+print("Fibonacci Series:")
+
+for i in range(n):
+    print(a, end=" ")
+    a, b = b, a + b
