cdbbbaa9328a4e9e0bfbd76e69d3bd6c9e1e2cbf
shreenidhitd123 <shreenidhitd123@gmail.com>
2026-07-03 00:24:19 +0530

    Create 25.py

---
+++ 25.py
@@ -0,0 +1,8 @@
+#Palindrome Checker using python programming language 
+
+text = input("Enter a string: ")
+
+if text == text[::-1]:
+    print("It is a palindrome.")
+else:
+    print("It is not a palindrome.")
