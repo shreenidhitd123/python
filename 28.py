7bd12d6d1eb9859e5fd72a839a0354d213de4552
shreenidhitd123 <shreenidhitd123@gmail.com>
2026-07-06 21:41:42 +0530

    Create 28.py

---
+++ 28.py
@@ -0,0 +1,11 @@
+#using python programming language 
+import random
+import string
+
+length = int(input("Enter password length: "))
+
+characters = string.ascii_letters + string.digits + string.punctuation
+
+password = "".join(random.choice(characters) for _ in range(length))
+
+print("Generated Password:", password)
