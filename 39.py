e518f157d4386289c9938b11cad01d14ecd80e53
shreenidhitd123 <shreenidhitd123@gmail.com>
2026-07-17 00:32:55 +0530

    Create 39.py

---
+++ 39.py
@@ -0,0 +1,12 @@
+#Count Vowels in a String
+
+text = input("Enter a string: ")
+
+count = 0
+vowels = "aeiouAEIOU"
+
+for ch in text:
+    if ch in vowels:
+        count += 1
+
+print("Number of vowels:", count)
