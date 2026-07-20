d95bceffcaf27744f05c32d25e5e4720f66c861b
shreenidhitd123 <shreenidhitd123@gmail.com>
2026-07-20 21:10:51 +0530

    Create 42.py

---
+++ 42.py
@@ -0,0 +1,8 @@
+#Find the Largest Element in a List using python programming language 
+
+numbers = [12, 45, 7, 89, 34, 56]
+
+largest = max(numbers)
+
+print("Numbers:", numbers)
+print("Largest number:", largest)
