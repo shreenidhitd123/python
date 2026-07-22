2910553ebf3eac39fcaea15f2131b5e80afcbc48
shreenidhitd123 <shreenidhitd123@gmail.com>
2026-07-22 23:44:34 +0530

    Create 45.py

---
+++ 45.py
@@ -0,0 +1,8 @@
+#Find Largest Number in a List using python programming language 
+
+numbers = [10, 25, 5, 40, 15]
+
+largest = max(numbers)
+
+print("Numbers:", numbers)
+print("Largest number:", largest)
