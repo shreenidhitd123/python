084b9650f017fe7cfc34639bbd34ffe40d0d6865
shreenidhitd123 <shreenidhitd123@gmail.com>
2026-07-29 21:13:02 +0530

    Create 52.py

---
+++ 52.py
@@ -0,0 +1,13 @@
+# Program to check whether a number is prime using python programming language 
+
+num = int(input("Enter a number: "))
+
+if num <= 1:
+    print(num, "is not a prime number")
+else:
+    for i in range(2, int(num ** 0.5) + 1):
+        if num % i == 0:
+            print(num, "is not a prime number")
+            break
+    else:
+        print(num, "is a prime number")
