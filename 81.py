5e9c033a439d165e54e137bb03d78a4189c983ca
shreenidhitd123 <shreenidhitd123@gmail.com>
2026-08-28 14:03:41 +0530

    Create 81.py

---
+++ 81.py
@@ -0,0 +1,11 @@
+# Program to reverse a number using python programming language 
+
+num = int(input("Enter a number: "))
+reverse = 0
+
+while num > 0:
+    digit = num % 10
+    reverse = reverse * 10 + digit
+    num //= 10
+
+print("Reversed number:", reverse)
