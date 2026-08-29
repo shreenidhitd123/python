eaf82b460b2fb2d2646c7764de8955d9f9818c6e
shreenidhitd123 <shreenidhitd123@gmail.com>
2026-08-29 11:43:00 +0530

    Create 82.py

---
+++ 82.py
@@ -0,0 +1,8 @@
+# Program to check whether a number is even or odd using python programming language 
+
+num = int(input("Enter a number: "))
+
+if num % 2 == 0:
+    print(num, "is an even number")
+else:
+    print(num, "is an odd number")
