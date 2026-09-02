683576dcf6649e7659b23f360f7220d5dad57979
shreenidhitd123 <shreenidhitd123@gmail.com>
2026-09-02 18:09:51 +0530

    Create 86.py

---
+++ 86.py
@@ -0,0 +1,12 @@
+# Program to count the number of vowels in a string using python programming language 
+
+text = input("Enter a string: ")
+
+vowels = "aeiouAEIOU"
+count = 0
+
+for char in text:
+    if char in vowels:
+        count += 1
+
+print("Number of vowels:", count)
