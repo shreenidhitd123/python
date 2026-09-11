18b73ded0aa6e34015c45eee01400207b1fa77eb
shreenidhitd123 <shreenidhitd123@gmail.com>
2026-09-11 17:28:50 +0530

    Create 95.py

---
+++ 95.py
@@ -0,0 +1,15 @@
+# Program to check whether a number is palindrome using python programming language 
+
+num = int(input("Enter a number: "))
+original = num
+reverse = 0
+
+while num > 0:
+    digit = num % 10
+    reverse = reverse * 10 + digit
+    num //= 10
+
+if original == reverse:
+    print(original, "is a palindrome number")
+else:
+    print(original, "is not a palindrome number")
