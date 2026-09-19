8f7cd6b76bb0e5a52d7ef76b354e162cc49fa7d9
shreenidhitd123 <shreenidhitd123@gmail.com>
2026-09-19 16:23:28 +0530

    Create 103.py

---
+++ 103.py
@@ -0,0 +1,11 @@
+# Program to count vowels in a string
+
+text = input("Enter a string: ")
+vowels = "aeiouAEIOU"
+count = 0
+
+for ch in text:
+    if ch in vowels:
+        count += 1
+
+print("Number of vowels:", count)
