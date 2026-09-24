df25b515ef4ecebb2a8198ec7aabb806343eed30
shreenidhitd123 <shreenidhitd123@gmail.com>
2026-09-24 22:08:26 +0530

    Create 108.py

---
+++ 108.py
@@ -0,0 +1,11 @@
+# Program to count vowels in a string using python programming language 
+
+text = input("Enter a string: ")
+
+count = 0
+
+for char in text:
+    if char.lower() in "aeiou":
+        count += 1
+
+print("Number of vowels:", count)
